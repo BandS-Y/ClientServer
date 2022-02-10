09f85c7f5b4c1727039cb2178344560348b010a8
BandS-Y <yuverch@gmail.com>
2022-02-10 14:27:22 +0700

    Lesson_02 1 Task

--- Lesson_02.py
+++ Lesson_02.py
@@ -14,34 +14,42 @@
 
 import chardet
 import re
+import csv
 
 file_names = ['info_1.txt', 'info_2.txt', 'info_3.txt']
-out_file = 'main_data'
+out_file = 'main_data.csv'
+
 
 # МОЙ КОМЕНТАРИЙ
 # если честно, то задание немного путанное, что и куда помещать до конца не ясно, поэтому буду делать, как придумал
+
+# def find_line(base_param, line):
+#     key_res = re.match(r'base_param, line)
+#     if key_res:
+#         res = re.findall(r':\s*(\w+.*)', line)
+#         return res[0]
 
 def file_read(file_names):
     os_prod_list = []
     os_name_list = []
     os_code_list = []
     os_type_list = []
-    main_data = ['Изготовитель системы', 'Название ОС', 'Код продукта', 'Тип системы']
+    main_data = [['Изготовитель системы', 'Название ОС', 'Код продукта', 'Тип системы']]
 
-    for file_name in file_names:
+    for file_name in file_names:  # определяем кодировку файла
         with open(file_name, 'rb') as f:
             content = f.read()
         encoding = chardet.detect(content)['encoding']
         # print(encoding)
 
-        with open(file_name, encoding=encoding) as f:
+        with open(file_name, encoding=encoding) as f:  # перебираем файлы, в каждом из которых ищем нужные записи.
             for line in f:
-                key_res = re.match(r'Изготовитель системы:', line)
-                if key_res:
-                    res = re.findall(r':\s*(\w+.*)', line)
-                    os_prod_list.append(res[0])
+                key_res = re.match(r'Изготовитель системы', line)  # хотел оформить в отдельную функцию, но слишком
+                if key_res:  # много передавать параметров получается
+                    res = re.findall(r':\s*(\w+.*)', line)  # ищем значение параметра
+                    os_prod_list.append(res[0])  # добавляем значение параметра в список
                 key_res = re.match(r'Название ОС:', line)
-                if  key_res:
+                if key_res:
                     res = re.findall(r':\s*(\w+.*)', line)
                     os_name_list.append(res[0])
                 key_res = re.match(r'Код продукта:', line)
@@ -52,10 +60,21 @@ def file_read(file_names):
                 if key_res:
                     res = re.findall(r':\s*(\w+.*)', line)
                     os_type_list.append(res[0])
-    # print(os_prod_list)
-    # print(os_name_list)
-    # print(os_code_list)
-    # print(os_type_list)
+
+    len_of_data = range(len(os_prod_list))
+    [main_data.append([]) for i in len_of_data]  # формируем список списков
+    for i in len_of_data:  # распределяем данные по столбцам
+        main_data[i + 1].append(os_prod_list[i])
+        main_data[i + 1].append(os_name_list[i])
+        main_data[i + 1].append(os_code_list[i])
+        main_data[i + 1].append(os_type_list[i])
+    return main_data
 
 
-file_read(file_names)
+def write_to_csv(file_name):
+    with open(file_name, 'w', encoding='utf-8') as f_n:
+        writer = csv.writer(f_n)
+        writer.writerows(file_read(file_names))
+
+
+write_to_csv(out_file)
