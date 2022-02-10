eb44f34362d46d4acc787e97756d3afce2e5e1e1
BandS-Y <yuverch@gmail.com>
2022-02-10 18:06:00 +0700

    Lesson_02 2 Task

--- Lesson_02.py
+++ Lesson_02.py
@@ -11,10 +11,12 @@
 # Создать функцию write_to_csv(), в которую передавать ссылку на CSV-файл. В этой функции реализовать получение данных
 # через вызов функции get_data(), а также сохранение подготовленных данных в соответствующий CSV-файл;
 # Проверить работу программы через вызов функции write_to_csv().
+import json
 
 import chardet
 import re
 import csv
+import yaml
 
 file_names = ['info_1.txt', 'info_2.txt', 'info_3.txt']
 out_file = 'main_data.csv'
@@ -22,12 +24,6 @@ out_file = 'main_data.csv'
 
 # МОЙ КОМЕНТАРИЙ
 # если честно, то задание немного путанное, что и куда помещать до конца не ясно, поэтому буду делать, как придумал
-
-# def find_line(base_param, line):
-#     key_res = re.match(r'base_param, line)
-#     if key_res:
-#         res = re.findall(r':\s*(\w+.*)', line)
-#         return res[0]
 
 def file_read(file_names):
     os_prod_list = []
@@ -40,7 +36,6 @@ def file_read(file_names):
         with open(file_name, 'rb') as f:
             content = f.read()
         encoding = chardet.detect(content)['encoding']
-        # print(encoding)
 
         with open(file_name, encoding=encoding) as f:  # перебираем файлы, в каждом из которых ищем нужные записи.
             for line in f:
@@ -68,7 +63,7 @@ def file_read(file_names):
         main_data[i + 1].append(os_name_list[i])
         main_data[i + 1].append(os_code_list[i])
         main_data[i + 1].append(os_type_list[i])
-    return main_data
+    return main_data  # отдаём готовые данные
 
 
 def write_to_csv(file_name):
@@ -77,4 +72,50 @@ def write_to_csv(file_name):
         writer.writerows(file_read(file_names))
 
 
-write_to_csv(out_file)
+# write_to_csv(out_file)
+
+
+# 2. Задание на закрепление знаний по модулю json. Есть файл orders в формате JSON с информацией о заказах. Написать
+# скрипт, автоматизирующий его заполнение данными. Для этого:
+# Создать функцию write_order_to_json(), в которую передается 5 параметров — товар (item), количество (quantity),
+# цена (price), покупатель (buyer), дата (date). Функция должна предусматривать запись данных в виде словаря в файл
+# orders.json. При записи данных указать величину отступа в 4 пробельных символа;
+# Проверить работу программы через вызов функции write_order_to_json() с передачей в нее значений каждого параметра.
+
+orders_empty = {'orders': []}  # создадим словарь, чтоб обнулить файл
+
+# перезапишем файл пустыми данными, чтоб он не переполнялся
+with open('orders.json', 'w', encoding='utf-8') as f_n:
+    json.dump(orders_empty, f_n, sort_keys=True, indent=4, ensure_ascii=False)
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    # открывваем и читаем данные из файла
+    with open('orders.json', encoding='utf-8') as f_n:
+        orders = json.load(f_n)
+
+    # добавляем новые данные
+    orders['orders'].append({'item': item, 'quantity': quantity, 'price': price, 'buyer': buyer, 'date': date})
+
+    # записываем новые данные
+    with open('orders.json', 'w', encoding='utf-8') as f_n:
+        json.dump(orders, f_n, sort_keys=True, indent=4, ensure_ascii=False)
+
+
+# последовательно заполняем файл данными
+write_order_to_json('pen', 10, 3.50, 'Petrov P.P.', '20.12.2020')
+write_order_to_json('pencil', 15, 4.50, 'Smirnov P.P.', '10.12.2020')
+write_order_to_json('unpen', 1, 599.99, 'Gaurgoff P.P.', '11.12.2021')
+write_order_to_json('penal', 100, 50.99, 'Trov P.P.', '01.12.2021')
+write_order_to_json('pencle', 12, 15.50, 'Petrov P.P.', '29.11.2020')
+
+
+# 3. Задание на закрепление знаний по модулю yaml. Написать скрипт, автоматизирующий сохранение данных в файле
+# YAML-формата. Для этого:
+# Подготовить данные для записи в виде словаря, в котором первому ключу соответствует список, второму — целое число,
+# третьему — вложенный словарь, где значение каждого ключа — это целое число с юникод-символом, отсутствующим в
+# кодировке ASCII (например, €);
+# Реализовать сохранение данных в файл формата YAML — например, в файл file.yaml. При этом обеспечить стилизацию
+# файла с помощью параметра default_flow_style, а также установить возможность работы с юникодом: allow_unicode = True;
+# Реализовать считывание данных из созданного файла и проверить, совпадают ли они с исходными.
+
